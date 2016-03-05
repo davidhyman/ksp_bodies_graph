235df1075da51ba716f31eabf50b9d5aec915dd9
davidhyman <nomail@dammitgithub.com>
2016-03-05 23:12:55 +0000

    add all the data

--- main.js
+++ main.js
@@ -7,11 +7,11 @@ const DV_MATCH = null
 
 // some modes for aero calculations
 var AERO_MODES = [
-    {key: 'entry_only', friendly: 'on entry', title: 'Only aero brake from orbit to surface'},
     {key: 'best', friendly: 'perfect', title: 'Optimal aero braking used on every manouvre'}, 
+    {key: 'entry_only', friendly: 'on entry', title: 'Only aero brake from orbit to surface'},
     {key: 'no_braking', friendly: 'never', title: 'Never aero brake'},
 ]
-var AERO_MODE = -1
+var AERO_MODE = 0
 
 // some modes for assuming a mimimum dv on aerobraking
 var AEROBRAKE_DV = 0
@@ -25,15 +25,30 @@ var node_pair = []
 // planet colours
 const DEFAULT_COLOUR = '#333'
 const PLANET_COLOUR = {
+    kerbol: '#ff0',
     kerbin: '#0AF',
     mun: '#666',
+    minmus: '#0fa',
+    eeloo: '#eee',
+    dres: '#aaa',
+    duna: '#a00',
+    ike: '#faa',
+    eve: '#a0a',
+    gilly: '#faf',
+    moho: '#fa0',
+    jool: '#060',
+    bop: '#090',
+    pol: '#0a0',
+    tylo: '#0c0',
+    vall: '#0a6',
+    laythe: '#0aa',
 }
 
 // ksp dv map, directions are relative to kerbin
 const NODE_SOURCE = [
     {source:'kerbin_surface', target:'kerbin_orbit', out: 3400, back: AEROBRAKE},
     {source:'kerbin_orbit', target:'kerbin_geostationary', out: 1115, back: AEROBRAKE},
-    {source:'kerbin_orbit', target:'kerbin_intercept', out: 950, back: AEROBRAKE},
+    {source:'kerbin_orbit', target:'kerbin_capture', out: 950, back: AEROBRAKE},
     
     {source:'kerbin_orbit', target:'mun_intercept', out: 860, back: AEROBRAKE},
     {source:'mun_intercept', target:'mun_orbit', out: 310, back: DV_MATCH},
@@ -42,6 +57,63 @@ const NODE_SOURCE = [
     {source:'kerbin_orbit', target:'minmus_intercept', out: 930, back: AEROBRAKE, plane_change: 340},
     {source:'minmus_intercept', target:'minmus_orbit', out: 160, back: DV_MATCH},
     {source:'minmus_orbit', target:'minmus_surface', out: 180, back: DV_MATCH},
+    
+    {source:'kerbin_orbit', target:'dres_intercept', out: 610, back: AEROBRAKE, plane_change: 1010},
+    {source:'dres_intercept', target:'dres_orbit', out: 1290, back: DV_MATCH},
+    {source:'dres_orbit', target:'dres_surface', out: 430, back: DV_MATCH},
+    
+    {source:'kerbin_orbit', target:'moho_intercept', out: 760, back: AEROBRAKE, plane_change: 2520},
+    {source:'moho_intercept', target:'moho_orbit', out: 2410, back: DV_MATCH},
+    {source:'moho_orbit', target:'moho_surface', out: 870, back: DV_MATCH},
+    
+    {source:'kerbin_capture', target:'kerbol_capture', out: 6000, back: AEROBRAKE},
+    {source:'kerbol_capture', target:'kerbol_orbit', out: 137000, back: DV_MATCH},
+    {source:'kerbol_orbit', target:'kerbol_surface', out: 67000, back: DV_MATCH},
+    
+    {source:'kerbin_capture', target:'eeloo_intercept', out: 1140, back: AEROBRAKE, plane_change: 1330},
+    {source:'eeloo_intercept', target:'eeloo_orbit', out: 1370, back: DV_MATCH},
+    {source:'eeloo_orbit', target:'eeloo_surface', out: 620, back: DV_MATCH},
+    
+    {source:'kerbin_capture', target:'eve_intercept', out: 90, back: AEROBRAKE, plane_change: 430},
+    {source:'eve_intercept', target:'eve_capture', out: AEROBRAKE, back: 80},
+    {source:'eve_capture', target:'eve_orbit', out: AEROBRAKE, back: 1330},
+    {source:'eve_orbit', target:'eve_surface', out: AEROBRAKE, back: 8000},
+    {source:'eve_capture', target:'gilly_intercept', out: 60, back: DV_MATCH},
+    {source:'gilly_intercept', target:'gilly_orbit', out: 410, back: DV_MATCH},
+    {source:'gilly_orbit', target:'gilly_surface', out: 30, back: DV_MATCH},
+    
+    {source:'kerbin_capture', target:'duna_intercept', out: 130, back: AEROBRAKE, plane_change: 10},
+    {source:'duna_intercept', target:'duna_capture', out: AEROBRAKE, back: 250},
+    {source:'duna_capture', target:'duna_orbit', out: AEROBRAKE, back: 360},
+    {source:'duna_orbit', target:'duna_surface', out: AEROBRAKE, back: 1450},
+    {source:'duna_capture', target:'ike_intercept', out: 30, back: DV_MATCH},
+    {source:'ike_intercept', target:'ike_orbit', out: 180, back: DV_MATCH},
+    {source:'ike_orbit', target:'ike_surface', out: 390, back: DV_MATCH},
+    
+    {source:'kerbin_capture', target:'jool_intercept', out: 980, back: AEROBRAKE, plane_change: 270},
+    {source:'jool_intercept', target:'jool_capture', out: AEROBRAKE, back: 160},
+    {source:'jool_capture', target:'jool_orbit', out: AEROBRAKE, back: 2810},
+    {source:'jool_orbit', target:'jool_surface', out: AEROBRAKE, back: 14000},
+    
+    {source:'jool_capture', target:'pol_intercept', out: 160, back: DV_MATCH, plane_change: 700},
+    {source:'pol_intercept', target:'pol_orbit', out: 820, back: DV_MATCH},
+    {source:'pol_orbit', target:'pol_surface', out: 130, back: DV_MATCH},
+    
+    {source:'jool_capture', target:'bop_intercept', out: 220, back: DV_MATCH, plane_change: 2440},
+    {source:'bop_intercept', target:'bop_orbit', out: 900, back: DV_MATCH},
+    {source:'bop_orbit', target:'bop_surface', out: 220, back: DV_MATCH},
+    
+    {source:'jool_capture', target:'tylo_intercept', out: 400, back: DV_MATCH},
+    {source:'tylo_intercept', target:'tylo_orbit', out: 1100, back: DV_MATCH},
+    {source:'tylo_orbit', target:'tylo_surface', out: 2270, back: DV_MATCH},
+    
+    {source:'jool_capture', target:'vall_intercept', out: 620, back: DV_MATCH},
+    {source:'vall_intercept', target:'vall_orbit', out: 910, back: DV_MATCH},
+    {source:'vall_orbit', target:'vall_surface', out: 860, back: DV_MATCH},
+    
+    {source:'jool_capture', target:'laythe_intercept', out: 930, back: DV_MATCH},
+    {source:'laythe_intercept', target:'laythe_orbit', out: 1070, back: DV_MATCH},
+    {source:'laythe_orbit', target:'laythe_surface', out: AEROBRAKE, back: 2900},
 ]
 
 function node_data(node_id){
@@ -215,27 +287,57 @@ function init(){
                 'line-style': 'dashed',
             })
     
-    var layout = {
-        name: 'cose',
-        gravity: 0,
-        numIter: 2000,
-        coolingFactor: 0.97,
-        nodeRepulsion: function( node ){ return 400000; },
-        idealEdgeLength: function( edge ){ return 3},
-        edgeElasticity: function( edge ){ return edge.data('no_braking') || 1 },
-    }
-    
+    //edge.data('no_braking') || 1 
     CY = cytoscape({
         container: $('#graph'),
         elements: get_data(),
         selectionType: 'additive',
         style: style,
-        layout: layout,
         autoungrabify: true,
         wheelSensitivity: 0.15,
     })
     
+    var layouts = {
+        'spring': {
+            name: 'cose',
+            gravity: 0,
+            numIter: 12000,
+            minTemp: 0.1,
+            coolingFactor: 0.98,
+            nodeRepulsion: function( node ){ return 100000; },
+            idealEdgeLength: function( edge ){ return 3},
+            edgeElasticity: function( edge ){ return 200*(edge.target().degree() + edge.source().degree()) },
+        },
+        'round': {
+            name: 'concentric',
+            startAngle: Math.PI * 9/8,
+            sweep: Math.PI * 6/8,
+            minNodeSpacing: 2,
+            equidistant: false,
+            concentric: function( node ){
+                var x = search_graph(CY, CY.$('#kerbin_surface'), node);
+                return -x.path.length
+            },
+            levelWidth: function( nodes ){ return 3},
+        },
+        'tree': {
+            name: 'breadthfirst',
+            roots: CY.$('#kerbin_surface'),
+        }
+    }
+    LAYOUT = 1
+    LAYOUTS = _.keys(layouts)
+    
     CY.on('tap', 'node', function(e) {on_tap_handler(CY,e)})
+    
+    $('#layout').click(function(){
+        // iterates the plane change dv inclusion
+        LAYOUT = (LAYOUT + 1) % LAYOUTS.length
+        $('#layout').text('Layout: ' + LAYOUTS[LAYOUT])
+        CY.layout(layouts[LAYOUTS[LAYOUT]])
+        // too cluttered with edges
+        CY.edges().style({opacity:LAYOUTS[LAYOUT] == 'round' ? 0.2 : 1})
+    })
       
     $('#dv_mode').click(function(){
         // iterates the aero mode
@@ -247,7 +349,7 @@ function init(){
     $('#aero_dv').click(function(){
         // iterates the aero dv assumption
         AEROBRAKE_DV = (AEROBRAKE_DV + 1) % AEROBRAKE_DVS.length
-        $('#aero_dv').text('Min AB DV: ' + AEROBRAKE_DVS[AEROBRAKE_DV]).prop('title', 'Assumed minimum delta v needed for any aerobraking manouvre')
+        $('#aero_dv').text('Min AB DV: ' + AEROBRAKE_DVS[AEROBRAKE_DV])
         search_and_render(CY)
     })
     
@@ -261,6 +363,7 @@ function init(){
     $('#aero_dv').click()
     $('#dv_mode').click()
     $('#plane_change').click()
+    $('#layout').click()
     
     var default_node = CY.$('#kerbin_surface')
     default_node.select()
